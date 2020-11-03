bbf46001c7bf41dbcedbc596d44a87721d7a6650
Owen Cruse <ocruse7@gmail.com>
2020-11-03 20:09:15 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,6 +3,7 @@ def position_taken?( board, index )
   if board[index] != " " || board[index] != "" || board[index] != nil
     return false
   end
-  
-  return true
+  elsif board[index] == "X" || board[index] == "O"
+    return true
+  end
 end
